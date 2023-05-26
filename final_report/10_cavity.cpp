ce58abfdd501accc69fd0f39f9d5e03cf6ed4b88
Koki Ishii <ereven0330@gmail.com>
2023-05-26 12:04:25 +0900

    final_report(並列化なし)

---
+++ final_report/10_cavity.cpp
@@ -0,0 +1,93 @@
+#include <vector>
+#include <cmath>
+
+int main() {
+    int nx = 41;
+    int ny = 41;
+    int nt = 500;
+    int nit = 50;
+    double dx = 2 / (nx - 1);
+    double dy = 2 / (ny - 1);
+    double dt = 0.01;
+    double rho = 1;
+    double nu = 0.02;
+
+    std::vector<double> x(nx, 0);
+    std::vector<double> y(ny, 0);
+
+    std::vector<std::vector<double>> u(ny, std::vector<double>(nx, 0));
+    std::vector<std::vector<double>> v(ny, std::vector<double>(nx, 0));
+    std::vector<std::vector<double>> p(ny, std::vector<double>(nx, 0));
+    std::vector<std::vector<double>> b(ny, std::vector<double>(nx, 0));
+
+    for (int i = 0; i < nx; i++) {
+        x[i] = i * dx;
+    }
+
+    for (int j = 0; j < ny; j++) {
+        y[j] = j * dy;
+    }
+
+    
+    for (int n = 0; n < nt; n++) {
+        for (int j = 1; j < ny-1; j++) {
+            for (int i = 1; i < nx-1; i++) {
+                b[j][i] = rho * (1 / dt *
+                                ((u[j][i+1] - u[j][i-1]) / (2 * dx)     +          (v[j+1][i] - v[j-1][i]) / (2 * dy)) -
+                        std::pow((u[j][i+1] - u[j][i-1]) / (2 * dx), 2) -     2 * ((u[j+1][i] - u[j-1][i]) / (2 * dy) *
+                                 (v[j][i+1] - v[j][i-1]) / (2 * dx))    - std::pow((v[j+1][i] - v[j-1][i]) / (2 * dy), 2));
+            }
+        }
+        for (int it = 0; it < nit; it++) {
+            std::vector<std::vector<double>> pn = p;
+            for (int j = 1; j < ny-1; j++) {
+                for (int i = 1; i < nx-1; i++) {
+                    p[j][i] = (dy * dy * (pn[j][i+1] + pn[j][i-1]) +
+                            dx * dx * (pn[j+1][i] + pn[j-1][i]) -
+                            b[j][i] * std::pow(dx, 2) * std::pow(dy, 2))
+                            / (2 * (std::pow(dx, 2) + std::pow(dy, 2)));
+                }
+            }
+            for (int j = 0; j < ny; j++) {
+                p[j][nx-1] = p[j][nx-2];
+            }
+            for (int i = 0; i < nx; i++) {
+                p[0][i] = p[1][i];
+            }
+            for (int j = 0; j < ny; j++) {
+                p[j][0] = p[j][1];
+            }
+            for (int i = 0; i < nx; i++) {
+                p[ny-1][i] = 0;
+            }
+        }
+        std::vector<std::vector<double>> un = u;
+        std::vector<std::vector<double>> vn = v;
+        for (int j = 1; j < ny-1; j++) {
+            for (int i = 1; i < nx-1; i++) {
+                u[j][i] = un[j][i] - un[j][i] * dt / dx * (un[j][i] - un[j][i-1])
+                                   - un[j][i] * dt / dy * (un[j][i] - un[j-1][i])
+                                   - dt / (2 * rho * dx) * (p[j][i+1] - p[j][i-1])
+                                   + nu * dt / std::pow(dx, 2) * (un[j][i+1] - 2 * un[j][i] + un[j][i-1])
+                                   + nu * dt / std::pow(dy, 2) * (un[j+1][i] - 2 * un[j][i] + un[j-1][i]);
+                v[j][i] = vn[j][i] - vn[j][i] * dt / dx * (vn[j][i] - vn[j][i-1])
+                                   - vn[j][i] * dt / dy * (vn[j][i] - vn[j-1][i])
+                                   - dt / (2 * rho * dx) * (p[j+1][i] - p[j-1][i])
+                                   + nu * dt / std::pow(dx, 2) * (vn[j][i+1] - 2 * vn[j][i] + vn[j][i-1])
+                                   + nu * dt / std::pow(dy, 2) * (vn[j+1][i] - 2 * vn[j][i] + vn[j-1][i]);
+            }
+        }
+        for (int j = 0; j < ny; j++) {
+            u[j][0] = 0;
+            u[j][nx-1] = 0;
+            v[j][0] = 0;
+            v[j][nx-1] = 0;
+        }
+        for (int i = 0; i < nx; i++) {
+            u[0][i] = 0;
+            u[ny-1][i] = 1;
+            v[0][i] = 0;
+            v[ny-1][i] = 0;
+        }
+    }
+}
